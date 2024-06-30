dc4fa7a41795bde0ed25c9a2ffda88a9f2e633e4
Ritzz <154762212+RitzzRiver@users.noreply.github.com>
2024-06-30 14:58:56 -0500

    Initial commit

---
+++ src/ColorScheme/SetColor.java
@@ -0,0 +1,40 @@
+package ColorScheme;
+
+import java.awt.Color;
+
+public class SetColor{
+    
+    // color extracted from https://coolors.co/ffffff-e4d1d1-fe6c90-87286a-452459-260d34
+
+    /**
+     * @apiNote sets color to #FFFFFF mostly used for window layout background.
+     */
+    public static final Color WHITE = new Color(255, 255, 255);
+
+    /**
+     * @apiNote
+     * sets color to #E4D1D1 mostly used for window shadow.
+     */
+    public static final Color PALE_PINK = new Color(228, 209, 209);
+
+    /**
+     * @apiNote sets color to #FE6C90 used for window close and window markups
+     */
+    public static final Color PINK = new Color(254, 108, 144);
+
+    /**
+     * @apiNote sets color to #87286A used for window close indentation
+     */
+    public static final Color MAGENTA = new Color(135, 40, 106);
+
+    /**
+     * @apiNote sets color to #452459 this is a placeholder bc that's Scheme default XD
+     */
+    public static final Color PURPLE = new Color(69, 36, 89);
+
+    /**
+     * @apiNote sets color to #260D34 mostly used for text
+     */
+    public static final Color DEEP_PURPLE = new Color(38, 13, 52);
+    
+}
