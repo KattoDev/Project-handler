97464c57250f73110694e6040df48669f89b34d4
Ritzz <154762212+RitzzRiver@users.noreply.github.com>
2024-06-30 20:43:34 -0500

    fix Cosmic Late color

--- src/ColorScheme/SetColor.java
+++ src/ColorScheme/SetColor.java
@@ -6,7 +6,7 @@ public class SetColor{
     
     // color extracted from https://coolors.co/fffae7-f2ebce-d9c4a9-8b6455-6d4641-594e4a-c86845-a44c38
      
-    public static final Color COSMIC_LATE = new Color(109, 70, 65);
+    public static final Color COSMIC_LATE = new Color(255, 249, 230);
     public static final Color EGGSHELL = new Color(242, 235, 206);
     public static final Color DESERT_SAND = new Color(217, 196, 169);
     public static final Color ROSE_TAUPE = new Color(139, 100, 85);
